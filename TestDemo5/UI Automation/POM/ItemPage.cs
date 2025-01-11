55234b8ce945c5a6ada5265c16182d35d56390db
rajesh <rajesh.hcst1@gmail.com>
2025-01-12 00:52:44 +0530

    Cleanup

--- TestDemo5/UI Automation/POM/ItemPage.cs
+++ TestDemo5/UI Automation/POM/ItemPage.cs
@@ -1,12 +1,14 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Threading;
 
 namespace TestDemo5.UI_Automation.POM
 {
     public class ItemPage
     {
         private IWebDriver driver;
-        //private By addToCartButton = By.XPath("//a[contains(@id,'atcBtn')]");
-        private By addToCartButton = By.Id("atcBtn_btn_1");
+        private By addToCartButton = By.XPath("//a[contains(@id,'atcBtn')]");
         private By cartCount = By.CssSelector("#gh-cart-n");
 
         public ItemPage(IWebDriver driver)
@@ -16,7 +18,25 @@ namespace TestDemo5.UI_Automation.POM
 
         public void AddToCart()
         {
+            string mainWindowHandle = driver.CurrentWindowHandle;
+
+            // Wait for the new window or tab to open
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(100));
+            wait.Until(driver => driver.WindowHandles.Count > 1);
+
+            // Switch to the new window
+            foreach (string handle in driver.WindowHandles)
+            {
+                if (handle != mainWindowHandle)
+                {
+                    driver.SwitchTo().Window(handle);
+                    break;
+                }
+            }
+
+            Thread.Sleep(1000);
             driver.FindElement(addToCartButton).Click();
+
         }
 
         public string GetCartCount()
